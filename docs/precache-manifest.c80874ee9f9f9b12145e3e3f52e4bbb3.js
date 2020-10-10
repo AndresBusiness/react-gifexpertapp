a9f35ae97e2c7a6c0045258b53790e9a7656190e
Andres Perez Alonso <andres-tic@hotmail.com>
2020-10-09 23:56:24 -0500

    docs agregado

---
+++ docs/precache-manifest.c80874ee9f9f9b12145e3e3f52e4bbb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "224706688fd7bf0c3c4613d50b6cd221",
+    "url": "/index.html"
+  },
+  {
+    "revision": "242fb917656eb9cb2d9d",
+    "url": "/static/css/main.4a3811e6.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "242fb917656eb9cb2d9d",
+    "url": "/static/js/main.8a0ab4bb.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
